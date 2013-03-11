a9712623ed15fb9f3d2bd88b10998cd9cbe457cb
Michał Piechowiak <misiek.piechowiak@gmail.com>
2013-03-11 10:36:43 +0100

    zalamanie swiatla

--- sledzenie.h
+++ sledzenie.h
@@ -3,6 +3,6 @@
 
 #include "scena.h"
 
-kolor sledz(scena* scena, promien* promien, int numer_odbicia);
+kolor sledz(scena* scena, promien* promien, int numer_odbicia, int numer_zalamania, float n);
 void generuj(scena* scena, int w, int h, kolor* bufor, unsigned AA);
 #endif
